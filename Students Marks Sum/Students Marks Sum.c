5c1806400ae29ea914a109932a1fcacdf9a87477
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-21 14:26:46 +0100

    by gacp

--- Students Marks Sum/Students Marks Sum.c
+++ Students Marks Sum/Students Marks Sum.c
@@ -10,25 +10,19 @@ int marks_summation(int* marks, int number_of_students, char gender)
 {
     int sum = 0x0;
     
-    (void) gender;
-    if (number_of_students > 0x2)
+    for (int i = 0x0; i < number_of_students ; i += 0x1)
     {
-        for (int i = 0x0; i < number_of_students ; i += 0x1)
+        if (gender == 'b')
         {
-            if (gender == 'b')
-            {
-                if (i % 0x2 == 0x0)
-                    sum += marks[i];
-            }
-            else if (gender == 'g')
-            {
-                if (i % 0x2 != 0x0)
-                    sum += marks[i];
-            }
+            if (i % 0x2 == 0x0)
+                sum += marks[i];
+        }
+        else if (gender == 'g')
+        {
+            if (i % 0x2 != 0x0)
+                sum += marks[i];
         }
     }
-    else
-        sum = 0x0;
     return (sum);
 }
 
