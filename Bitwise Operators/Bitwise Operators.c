ff95a3255466798665697cb1efaff96bf828da1f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 17:04:25 +0100

    by gacp

--- Bitwise Operators/Bitwise Operators.c
+++ Bitwise Operators/Bitwise Operators.c
@@ -6,3 +6,21 @@
 
 
 
+void calculate_the_maximum(int n, int k)
+{
+	
+}
+
+int main()
+{
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
+
+
+
+
