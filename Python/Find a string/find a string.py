e92cd23eb84e2a35a49f3f6acf2d32390940959e
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-07-15 18:21:09 +0100

    find a string

---
+++ Python/Find a string/find a string.py
@@ -0,0 +1,19 @@
+def count_substring(string, sub_string):
+    li = 0
+    i = 0
+
+    while i <= len(string) - len(sub_string):
+        j = 0
+        while j < len(sub_string) and string[i + j] == sub_string[j]:
+            j += 1
+        if j == len(sub_string):
+            li += 1
+        i += 1
+    return li
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
