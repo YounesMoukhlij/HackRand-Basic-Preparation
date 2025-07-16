b49a6a7a4e84b86e36e174aa69f3cc0c0c3f8834
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-07-16 14:03:37 +0100

    find a string

---
+++ Python/sWAP caSE/sWAP caSE.py
@@ -0,0 +1,18 @@
+def swap_case(s):
+
+    result = ""
+    i = 0
+    while i < len(s):
+        if s[i] >= 'a' and s[i] <= 'z':
+            result += chr(ord(s[i]) - 32)
+        elif s[i] >= 'A' and s[i] <= 'Z':
+            result += chr(ord(s[i]) + 32)
+        else:
+            result += s[i]
+        i += 1
+    return result
+
+if __name__ == '__main__':
+    s = "HackerRank.com presents \"Pythonist 2\"."
+    result = swap_case(s)
+    print(result)
