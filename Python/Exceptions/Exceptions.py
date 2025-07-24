d8b61896bed73213ffcb04953bf33191f2d4a186
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-07-24 19:53:39 +0100

    Exception exercice

--- Python/Exceptions/Exceptions.py
+++ Python/Exceptions/Exceptions.py
@@ -1,9 +1,28 @@
-if "__name__" == "__main__":
 
-    i = input()
+if __name__ == "__main__":
 
-    print(i)
+    o = int(input())
 
-    # listed = list(map(int, input().split()))
+    all_cases = []
 
-    # print(listed)
+    for _ in range(o):
+        ar = list(map(str, input().split()))
+        all_cases.append(ar)
+
+    i = 0
+    while i < o:
+        try:
+            case = all_cases[i]
+            a, b = case[0], case[1]
+            if int(b) == 0:
+                raise ZeroDivisionError("Error Code: integer division or modulo by zero")
+            if  not a.isdigit():
+                raise ValueError(f"invalid literal for int() with base 10: '{a}'")
+            if  not b.isdigit():
+                raise ValueError(f"invalid literal for int() with base 10: '{b}'")
+            print(int(a) // int(b))
+        except ZeroDivisionError as e:
+            print("Error Code: integer division or modulo by zero")
+        except ValueError as e:
+            print("Error Code: " + str(e))
+        i += 1
